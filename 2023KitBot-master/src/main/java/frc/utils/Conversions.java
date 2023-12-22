facd0382b29167d344b59ba316c5310874a48289
RIGGZHyper <radmehrtp@gmail.com>
2023-12-21 17:26:12 -0800

    Set teleopDrive as default command
    Set teleopDrive command to control drivetrain

--- 2023KitBot-master/src/main/java/frc/utils/Conversions.java
+++ 2023KitBot-master/src/main/java/frc/utils/Conversions.java
@@ -2,8 +2,8 @@ package frc.utils;
 
 public class Conversions {
     public static double encoderTicksToRPM(double tickRate, double gearRatio) {
-        double motorRPM = tickRate * (500.0/2048.0);
-        double RPM = motorRPM/gearRatio;
+        double motorRPM = tickRate * (500.0 / 2048.0);
+        double RPM = motorRPM / gearRatio;
         return RPM;
     }
 
